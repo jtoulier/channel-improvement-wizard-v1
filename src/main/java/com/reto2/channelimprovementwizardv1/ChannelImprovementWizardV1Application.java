05a4dd97e571d2e1f5091ee68633a3ad90dbec79
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-02-07 23:51:18 -0500

    Empezando

---
+++ src/main/java/com/reto2/channelimprovementwizardv1/ChannelImprovementWizardV1Application.java
@@ -0,0 +1,13 @@
+package com.reto2.channelimprovementwizardv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelImprovementWizardV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelImprovementWizardV1Application.class, args);
+	}
+
+}
